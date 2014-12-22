1fbe427b3b9b01cfb8d9b427628e43bd5800c747
jorgechpugr <jorgechp@gmail.com>
2014-12-22 11:32:53 +0100

    Anadiendo los archivos iniciales del repositorio

---
+++ proyectoFinal/src/proyectofinal.py
@@ -0,0 +1,9 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+__author__="jorge"
+__date__ ="$22-dic-2014 11:28:44$"
+
+if __name__ == "__main__":
+    print "Hello World"
